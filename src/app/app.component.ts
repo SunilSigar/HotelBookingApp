1c497a3b75519500df36c369d72424c00171643d
Training_b6b.01.16 <Training_b6b.01.16@accenture.com>
2017-12-01 16:01:24 +0530

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  pagetitle = 'Welcome to UI case study for Hotel Booking';
+  description = 'This sample demonstrates how to use Angular and Bootstrap to develop a Single Page Application ';
+  desctitle = 'The key features:';
+  contents = ['Angular as MVC Framework', 'Bootstrap', 'REST', 'Jasmine'];
+  searchcasestudy = 'Start Case Study';
+
+  searchstring ='Search Hotels';
+  searchText ='Bengaluru';
+  MaximumResult = '5';
+
+
+  findHotels(){
+      
+  }
 }
